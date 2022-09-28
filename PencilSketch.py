674671a7062713e3d5ead202b47468304ab83de4
Thilagavath <113089789+Thilagavath@users.noreply.github.com>
2022-09-28 21:29:07 +0530

    Update PencilSketch.py

--- PencilSketch.py
+++ PencilSketch.py
@@ -1,1 +1,18 @@
-
+import cv2
+image = cv2.imread("me.jpeg")#imread-string representing the path of the image to be read.
+cv2.imshow("Me", image)#imshow-string representing the name of the window in which image to be displayed. 
+cv2.waitKey(0)#waitkey-Waits untill any key is pressed
+gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+cv2.imshow("Gray Me", gray_image)
+cv2.waitKey(0)
+inverted_image = 255 - gray_image
+cv2.imshow("Inverted", inverted_image)
+cv2.waitKey()
+blurred = cv2.GaussianBlur(inverted_image, (21, 21), 0)
+inverted_blurred = 255 - blurred
+pencil_sketch = cv2.divide(gray_image, inverted_blurred, scale=256.0)
+cv2.imshow("Sketch", pencil_sketch)
+cv2.waitKey(0)
+cv2.imshow("original image", image)
+cv2.imshow("pencil sketch", pencil_sketch)
+cv2.waitKey(0)
